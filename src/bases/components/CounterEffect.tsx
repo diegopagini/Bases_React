c2a9d22a7a3b2467ce177fa2dd2e4c687bccdbe9
Diego Pagini <diegopaginidev@gmail.com>
2022-11-13 14:40:47 +0100

    gsap

--- src/bases/components/CounterEffect.tsx
+++ src/bases/components/CounterEffect.tsx
@@ -1,4 +1,5 @@
 /** @format */
+import { gsap } from 'gsap';
 import { useEffect, useState } from 'react';
 
 const MAXIMUN_COUNT = 10;
@@ -12,6 +13,15 @@ export const CounterEffect = ({ initialValue = 0 }) => {
 		if (counter < MAXIMUN_COUNT) return;
 
 		console.log('%cSe alcanzó el valor máximo.', 'color: red; background-color: black'); // Con %c al principicio del string y luego las propiedades de css se puede estilizar la consola
+
+		// Animación
+		gsap.to('h2', { y: -10, duration: 0.25, ease: 'ease.out' }).then(() => {
+			gsap.to('h2', {
+				y: 0,
+				duration: 1,
+				ease: 'bounce.out',
+			});
+		});
 	}, [counter]); // El arreglo de dependencias que vuelven a activar el useEffect.
 
 	const handleClick = () => {
@@ -21,7 +31,8 @@ export const CounterEffect = ({ initialValue = 0 }) => {
 
 	return (
 		<>
-			<h1>CounterEffect: {counter}</h1>
+			<h1>CounterEffect:</h1>
+			<h2>{counter}</h2>
 			<button onClick={handleClick}>+1</button>
 		</>
 	);
